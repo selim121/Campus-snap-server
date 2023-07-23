83cd3a1f5bff76368d01afd0eb3744be354226e8
selim121 <selimkhan.bdkhan@gmail.com>
2023-07-23 23:25:45 +0600

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d18ofon.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -34,7 +34,6 @@ async function run() {
             const result = await collegesCollection.find({ rating: { $gte: minRating } }).toArray();
 
             res.json(result);
-
         })
 
         app.get('/colleges', async (req, res) => {
@@ -42,7 +41,12 @@ async function run() {
             res.send(result);
         })
 
-        
+        app.get('/colleges/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: id };
+            const result = await collegesCollection.findOne(query);
+            res.send(result);
+        })
 
 
 
